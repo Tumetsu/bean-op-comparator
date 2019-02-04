790b48fa97d52a63cc9e09578aebac0d14ac9833
Tuomas Salmi <salmi.tuomas@gmail.com>
2019-02-04 21:51:21 +0200

    Read data from Beancount and OP ledgers

---
+++ main.py
@@ -0,0 +1,34 @@
+import subprocess
+import csv
+from io import StringIO
+
+
+def read_beancount_ledger(file_path, start_date, end_date):
+    result = subprocess.run(['bean-query', '-f', 'csv', file_path, 'select date, narration, account, position where account ~ ".:Käyttötili" and date >= {} and date <= {};'.format(start_date, end_date)], stdout=subprocess.PIPE)
+    reader = csv.DictReader(StringIO(result.stdout.decode('utf-8')))
+
+    for row in reader:
+        row['position'] = float(row['position'].replace('EUR', '').strip())
+
+    return reader
+
+
+def read_op_ledger(file_path):
+    parsed = []
+    with open(file_path, 'r', encoding='iso-8859-1') as file:
+        reader = csv.DictReader(file, delimiter=';')
+
+        for row in reader:
+            parsed.append({
+                'date': row['Kirjauspäivä'],
+                'position': float(row['Määrä\xa0 EUROA'].replace(',', '.')),
+                'narration': row['Saaja/Maksaja']
+
+            })
+
+    return parsed
+
+ledger_log = read_beancount_ledger('~/ledger/ledger.beancount', '2019-01-01', '2019-01-31')
+op_log = read_op_ledger('./sample_data/op_ledger.csv')
+
+
