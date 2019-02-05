ba212a908859279df92f2c17671026973518b385
Tuomas Salmi <salmi.tuomas@gmail.com>
2019-02-05 21:56:36 +0200

    Create module from the script

--- op_bean_compare/__init__.py
+++ op_bean_compare/__init__.py
@@ -1,12 +1,8 @@
-#!/usr/bin/env python3
-
 import subprocess
 import csv
 from tabulate import tabulate
 from io import StringIO
 import datetime
-import argparse
-import sys
 
 
 def read_beancount_ledger(file_path, start_date, end_date):
@@ -105,21 +101,3 @@ def print_comparison_report(report):
         print(warning_color, 'No matches in ledger for transactions {}!\n'.format(strict_mode), end_color)
         print(tabulate(report['missing_transactions'], headers='keys'))
         exit(1)
-
-
-parser = argparse.ArgumentParser(description='Compare OP and Beancount ledgers and report differences')
-parser.add_argument('beancount', type=str, nargs=1, help='Path the to OP-ledger file')
-parser.add_argument('op', type=str, nargs=1, help='Path the to OP-ledger csv')
-parser.add_argument('start', type=str, nargs=1, help='Starting date as ISO-date')
-parser.add_argument('end', type=str, nargs=1, help='Ending date as ISO-date')
-parser.add_argument('--strict', type=bool, nargs='?', default=False, help='Enable strict mode')
-
-if __name__ == '__main__':
-    arguments = sys.argv[1:]
-    parsed_args = parser.parse_args(arguments)
-
-    ledger_log = read_beancount_ledger(parsed_args.beancount[0], parsed_args.start[0], parsed_args.end[0])
-    op_log = read_op_ledger(parsed_args.op[0])
-
-    comparison_report = compare_ledgers(op_log, ledger_log, parsed_args.strict)
-    print_comparison_report(comparison_report)
